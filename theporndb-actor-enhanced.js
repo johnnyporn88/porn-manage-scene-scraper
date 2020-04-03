4ec23fe9331ef230895a8729b50ddb61896d539e
johnnyporn88 <34014562+johnnyporn88@users.noreply.github.com>
2020-04-02 22:11:40 -0400

    Updated for new data style, added FreeOnes scrape
    Updated to be congruent with new TPDB data style (height, weight, etc.)
    Added FreeOnes scrape to get around inconsistencies with TPDB data on birth place, nationality, and heritage.

--- theporndb-actor-enhanced.js
+++ theporndb-actor-enhanced.js
@@ -2,56 +2,108 @@
 //Smashed together by john4valor.
 
 //This plugin will populate custom fields (if setup correctly) using metadata from ThePornDB.
+//Additionally, for certain metadata, it'll pull from Freeones instead for consistency.
 //DON'T fuck with anything above line 35.
 
-async ({ $axios, $log, actorName, $createImage }) => {
+async ({ event, args, $axios, $throw, $cheerio, $log, actorName, $createImage }) => {
 
-  $log(`Scraping ThePornDB for ${actorName}...`)
+  //basic setup check
+  if (event != "actorCreated" && event != "actorCustom")
+    $throw("Nope. This plugin is only meant for actorCreated or actorCustom events.");
+
+  $log(`Scraping ThePornDB and FreeOnes for ${actorName}...`)
 
   $log('Searching ThePornDB for ' + actorName)
   $log('https://master.metadataapi.net/api/performers?q=' + actorName)
 
-  const results = await $axios.get('https://metadataapi.net/api/performers?q=' + encodeURIComponent(actorName))
+  const results = await $axios.get('https://master.metadataapi.net/api/performers?q=' + encodeURIComponent(actorName))
 
   if (results.data.data.length === 0) {
-    $log('No results found')
+    $log('No results found on ThePornDB')
     return {}
   }
 
   if (results.data.data.length > 1) {
-    $log('Too many results found')
+    $log('Too many results found on ThePornDB')
     return {}
   }
 
-  const performerResults = await $axios.get('https://metadataapi.net/api/performers/' + results.data.data[0].id)
+  const performerResults = await $axios.get('https://master.metadataapi.net/api/performers/' + results.data.data[0].id)
   const performer = performerResults.data.data
   $log('Found results for ' + performer.id)
 
-  let thumbnailFile;
+  let thumbnailFile; //COMMENT OR DELETE OUT TO DISABLE PIC DOWNLOAD
 
-  if(performer.image){
-    thumbnailFile = await $createImage(performer.image, performer.id)
+  if(performer.image){ //COMMENT OR DELETE OUT TO DISABLE PIC DOWNLOAD
+    thumbnailFile = await $createImage(performer.image, performer.id) //COMMENT OR DELETE OUT TO DISABLE PIC DOWNLOAD
+  } //COMMENT OR DELETE OUT TO DISABLE PIC DOWNLOAD
+  
+  //defining FreeOnes URL for scraping
+  $log('Searching FreeOnes for ' + actorName)
+  const url = `https://freeones.xxx/${actorName.replace(/ /g, "-")}/profile`;
+  $log("Getting " + url);
+  //getting raw HTML of the URL defined above
+  const html = (await $axios.get(url)).data;
+  //getting DOM (Document Object Model) of the HTML
+  const dom = $cheerio.load(html);
+  
+  //physical properties extraction. Change 0.033 and 2.2 to 1 if metric is preferred.
+  const tallness_act = performer.extras.height.split("c")[0].trim() * 0.033; //extracts metric height measurements, and converts to imperial.
+  const fatness_ext = performer.extras.weight.split("k")[0].trim() * 2.2; //extracts metric weight measurements, and converts to imperial.
+  const fatness_act = Math.round((fatness_ext + Number.EPSILON) * 100) / 100; //rounding weight value.
+  
+  const startyr = performer.extras.first_seen.substr(0,4); //removes month, day, and time stamp of career start.
+  
+  //grabbing city of birth
+  const bcity_sel = dom('[data-test="section-personal-information"] a[href*="placeOfBirth"]');
+  const bcity_name = bcity_sel.length ? dom(bcity_sel).attr("href").split("=").slice(-1)[0] : null;
+  //if no city is found
+  if (!bcity_name) {
+	  $log('No city data found');
+  }
+
+  //grabbing state of birth
+  const bstate_sel = dom('[data-test="section-personal-information"] a[href*="province"]');
+  const bstate_name = bstate_sel.length ? dom(bstate_sel).attr("href").split("=").slice(-1)[0] : null;
+  //if no state is found
+  if (!bstate_name) {
+	  $log('No state data found');
+  }  
+  
+  const bplace = bcity_name + ', ' + bstate_name;
+  
+  //grabbing nation of birth
+  const bnation_sel = dom('[data-test="section-personal-information"] a[href*="country%5D"]');
+  const bnation_name = bnation_sel.length ? dom(bnation_sel).attr("href").split("=").slice(-1)[0] : null;
+  //if no state is found
+  if (!bnation_name) {
+	  $log('No nation data found');
   }
   
-  const tallness = performer.extras.height.split("(")[0].trim(); //extracts imperial height measurements only
-  const fatness = performer.extras.weight.split("l")[0].trim(); //extracts imperial weight measurements only
-  
   //vital stats extraction
-  const bewbs = performer.extras.cupsize.substr(2,1); //extracts cup size letter (e.g., A, B, etc.) from cupsize
+  const chst = performer.extras.measurements.split("-")[0].substr(0,2); //extracts chest measurement
+  const cup = performer.extras.measurements.split("-")[0].substr(2); //extracts cup size letter (e.g., A, B, etc.) from cupsize
+  const wst = performer.extras.measurements.split("-")[1].substr(0,2); //extracts waist measurement
+  const hps = performer.extras.measurements.split("-")[2].substr(0,2); //extracts hip measurement
+  
+  const astro = performer.extras.astrology.split(" ")[0].trim(); //astrological sign data clean up
   
   //The key value names below (e.g., "Astrology") MUST match the Custom Field names in Porn Manager.
   const customFields = {
-    Astrology: performer.extras.astrology, //String field
+    Astrology: astro, //String field
 	Gender: performer.extras.gender, //Single Choice field
 	Ethnicity: performer.extras.ethnicity, //Single Choice field
+	Heritage: performer.extras.nationality, //String field
 	'Hair Color': performer.extras.hair_colour, //Single Choice field
-	Height: tallness, //Number field
-	Weight: fatness, //Number field
-	Cupsize: bewbs, //Single Choice field
-	Measurements: performer.extras.measurements, // String field
-	Nationality: performer.extras.nationality, //Single Choice field
-	Birthplace: performer.extras.birthplace, //String field
-	Started: performer.extras.first_seen, //Number field
+	Height: tallness_act, //Number field.
+	Weight: fatness_act, //Number field.
+	'Chest Size': chst, //Number field
+	'Cup Size': cup, //Single Choice field
+	'Waist Size': wst, //Number field
+	'Hip Size': hps, //Number field
+	Nationality: bnation_name, //Single Choice field
+	Birthplace: bplace, //String field
+	Started: startyr, //Number field
 	Active: performer.extras.active //Boolean field
  }
 
@@ -63,7 +115,7 @@ async ({ $axios, $log, actorName, $createImage }) => {
   $log(customFields)
   
   return {
-    thumbnail: thumbnailFile,
+    thumbnail: thumbnailFile, //COMMENT OR DELETE OUT TO DISABLE PIC DOWNLOAD
     name: performer.name,
     bornOn: performer.extras.birthday_timestamp * 1000,
     aliases: performer.aliases,
